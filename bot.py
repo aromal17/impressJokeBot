b2ab8d589fa110710f2cc795a8e17d76f39913b2
aromal17 <aromalbenny17.com>
2021-09-20 18:27:48 +0530

    added no of clicks for per user basis

--- bot.py
+++ bot.py
@@ -6,11 +6,8 @@ import pandas as pd
 from decouple import config
 import psycopg2
 
-
-
 API_KEY = config('API_KEY')
 DATABASE_URL = config('DB_URL')
-
 bot = telegram.Bot(token=API_KEY)
 
 print("bot started")
@@ -30,10 +27,6 @@ def update_clicks(new_cnt, joke_type):
         update_query = """UPDATE clicks SET count = %s WHERE button_name = %s"""
         cur.execute(update_query,(new_cnt, joke_type))
         con.commit()
-        print("after") 
-        query = f"""SELECT * FROM clicks"""
-        results = pd.read_sql(query, con)
-        print(results)
         cur.close()
         con.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -52,24 +45,50 @@ def get_count(query):
         print(error)
     return new_cnt
 
+def update_user_data(user_name):
+    try:
+        con = psycopg2.connect(DATABASE_URL)
+        cur = con.cursor()
+        query = """SELECT count FROM user_details WHERE user_name = '{}'"""
+        results = pd.read_sql_query(query.format(user_name), con)
+        if results.empty:
+            insert_query = """INSERT INTO user_details (user_name, count) VALUES (%s, %s)"""
+            cur.execute(insert_query,(user_name, 1))
+            con.commit()
+        else:
+            new_cnt = int(results['count'].values[0]) + 1
+            update_query = """UPDATE user_details SET count = %s WHERE user_name = %s"""
+            cur.execute(update_query,(new_cnt, user_name))
+            con.commit()
+        cur.close()
+        con.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
 def handle_message(update, context):
     joke_type= str(update.message.text).lower()
-    print(joke_type)
+    user_name = update['message']['chat']['username']
     if joke_type == 'stupid':
         query = f"""SELECT count FROM clicks WHERE button_name = 'stupid'"""
         new_cnt = get_count(query)
         update_clicks(new_cnt, joke_type)
         update.message.reply_text(f"This is a joke from STUPID category")  
+        update_user_data(user_name)
+
     elif joke_type == 'fat':
         query = f"""SELECT count FROM clicks WHERE button_name = 'fat'"""
         new_cnt = get_count(query)
         update_clicks(new_cnt, joke_type)
         update.message.reply_text(f"This is a joke from FAT category")
+        update_user_data(user_name)
+
     elif joke_type == 'dumb':
         query = f"""SELECT count FROM clicks WHERE button_name = 'dumb'"""
         new_cnt = get_count(query)
         update_clicks(new_cnt, joke_type)
         update.message.reply_text(f"This is a joke from DUMB category")
+        update_user_data(user_name)
+
     elif joke_type == '/start':
         update.message.reply_text(f"Hey {update['message']['chat']['first_name']}. Hope you are doing great!!! \nWelcome to Impress_Jokes_Bot 🚀")
     send_options()
@@ -79,41 +98,7 @@ if __name__ == '__main__':
     updater = Updater(API_KEY, use_context=True)
     dp = updater.dispatcher
     send_options()
-   
-    # con = psycopg2.connect(DATABASE_URL)
-    # cur = con.cursor()
-    # # joke='stupid'
-    # cur.close()
-    try:
-        con = psycopg2.connect(DATABASE_URL)
-        cur = con.cursor()
-        # joke_id='stupid'
-        # count_no = 10
-
-        query = f"""SELECT * FROM clicks"""
-        results = pd.read_sql(query, con)
-        print("before")
-        print(results)
-
-        # query = f"""SELECT count FROM clicks WHERE button_name = 'stupid'"""
-        # results = pd.read_sql(query, con)
-        # curr_cnt = int(results['count'].values[0]) + 1
-        # update_query = """UPDATE clicks SET count = %s WHERE button_name = %s"""
-        # cur.execute(update_query,(curr_cnt, joke_id))
-        # con.commit()
-
-        # query = f"""SELECT * FROM clicks"""
-        # results = pd.read_sql(query, con)
-        # print(results)
-        # # cnt = results['count'].values[0]
-        # print(curr_cnt)
-        cur.close()
-        con.close()
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    
     dp.add_handler(MessageHandler(Filters.text, handle_message))
-    
+
     updater.start_polling(1.0)
     updater.idle()
